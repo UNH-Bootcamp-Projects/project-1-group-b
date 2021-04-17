ce5c8980ca07bbba9dc0524a99edd284902e52d7
Brendan Tyler <bkrendan12@gmail.com>
2021-04-17 12:55:09 -0400

    console log

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,28 +1,32 @@
 // variables
 var submitBtn = $(".submitBtn");
-var searchInputEl = $(".search-input");
+var searchInputEl = $(".searchInput");
 var datePickerEl = $(".date-picker");
 var dropDownEl = $(".dropdown-content");
 
 // functions
 
-function getData(event) {
+function getData(cityName) {
   // fetch both API's
-  fetch(
-    `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=D9il0k2ZQ5sNHnlsKYHApQEcivKsruvn`
-  ).then(function (responseTicket) {
-    return responseTicket.json();
-  }).then(ticketData) {
-    
-  }
+  return new Promise(function (resolve, reject) {
+    fetch(
+      `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=${cityName}&apikey=D9il0k2ZQ5sNHnlsKYHApQEcivKsruvn`
+    )
+      .then(function (responseTicket) {
+        return responseTicket.json();
+      })
+      .then(function (ticketData) {
+        console.log(ticketData._embedded.events[0]);
+      });
 
-  fetch(
-    `http://api.weatherstack.com/43c0e4c4f816398c7b97a3b59817de2b`
-  ).then(function (responseWeather) {
-    return responseWeather.json();
-  }).then(weatherData) {
-    
-  }
+    // fetch(`http://api.weatherstack.com/43c0e4c4f816398c7b97a3b59817de2b`)
+    //   .then(function (responseWeather) {
+    //     return responseWeather.json();
+    //   })
+    //   .then(function (weatherData) {
+    //     console.log(weatherData);
+    //   });
+  });
 }
 
 getData();
