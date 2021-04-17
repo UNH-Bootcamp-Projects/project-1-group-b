6bd4811c0d3a4d2441b47facf1015775c4ad15cb
Luke Teal <l.teal21@gmail.com>
2021-04-17 12:50:40 -0400

    Update weather api

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,28 +1,34 @@
 // variables
 var submitBtn = $(".submitBtn");
-var searchInputEl = $(".search-input");
-var datePickerEl = $(".date-picker");
-var dropDownEl = $(".dropdown-content");
+var searchInputEl = $(".searchInput");
+var datePickerEl = $(".datePicker");
+var dropDownEl = $(".dropdownContent");
 
 // functions
 
 function getData(event) {
+  event.preventDefault()
   // fetch both API's
-  fetch(
-    `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=D9il0k2ZQ5sNHnlsKYHApQEcivKsruvn`
-  ).then(function (responseTicket) {
-    return responseTicket.json();
-  }).then(ticketData) {
-    
-  }
+  // fetch(
+  //   `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=D9il0k2ZQ5sNHnlsKYHApQEcivKsruvn`
+  // ).then(function (responseTicket) {
+  //   return responseTicket.json();
+  // }).then(ticketData) {
+
+  // }
 
   fetch(
-    `http://api.weatherstack.com/43c0e4c4f816398c7b97a3b59817de2b`
-  ).then(function (responseWeather) {
+    'http://api.weatherstack.com/forecast?access_key=43c0e4c4f816398c7b97a3b59817de2b&query='+encodeURIComponent('New York')+'&forecast_days=1'
+  )
+  .then(function (responseWeather) {
     return responseWeather.json();
-  }).then(weatherData) {
-    
-  }
+  })
+  .then(function (weatherData){
+    console.log(weatherData);
+  }) 
 }
 
-getData();
+
+// getData();
+
+submitBtn.on('click',getData)
