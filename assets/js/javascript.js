839fb178e93eab152bb6c156fac42695e1a4558a
Luke Teal <l.teal21@gmail.com>
2021-04-17 12:51:25 -0400

    Update javascript.js

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -9,13 +9,13 @@ var dropDownEl = $(".dropdownContent");
 function getData(event) {
   event.preventDefault()
   // fetch both API's
-  // fetch(
-  //   `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=D9il0k2ZQ5sNHnlsKYHApQEcivKsruvn`
-  // ).then(function (responseTicket) {
-  //   return responseTicket.json();
-  // }).then(ticketData) {
+  fetch(
+    `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=D9il0k2ZQ5sNHnlsKYHApQEcivKsruvn`
+  ).then(function (responseTicket) {
+    return responseTicket.json();
+  }).then(ticketData) {
 
-  // }
+  }
 
   fetch(
     'http://api.weatherstack.com/forecast?access_key=43c0e4c4f816398c7b97a3b59817de2b&query='+encodeURIComponent('New York')+'&forecast_days=1'
