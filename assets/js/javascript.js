9bc5768a684870c70b0d7ba453e1502645bbc00c
Luke Teal <l.teal21@gmail.com>
2021-04-17 10:45:25 -0400

    Add weather API

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -10,9 +10,19 @@ function getData(event) {
   // fetch both API's
   fetch(
     `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=D9il0k2ZQ5sNHnlsKYHApQEcivKsruvn`
-  ).then(function (response) {
-    return response.json();
-  });
+  ).then(function (responseTicket) {
+    return responseTicket.json();
+  }).then(ticketData) {
+    
+  }
+
+  fetch(
+    `http://api.weatherstack.com/43c0e4c4f816398c7b97a3b59817de2b`
+  ).then(function (responseWeather) {
+    return responseWeather.json();
+  }).then(weatherData) {
+    
+  }
 }
 
 getData();
