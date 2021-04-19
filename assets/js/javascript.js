9c15957a1978aa896c6b17496f7b301025fea54e
Brendan Tyler <bkrendan12@gmail.com>
2021-04-18 23:55:03 -0400

    cleaned up position and styling and used have the Ticketmaster API appending data to the page

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,37 +1,52 @@
 // variables
 var submitBtn = $(".submitBtn");
 var searchInputEl = $(".searchInput");
-
-// functions
-
-function getData(cityName) {
-  // fetch both API's
-  return new Promise(function (resolve, reject) {
-    fetch(
-      `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=${cityName}&apikey=D9il0k2ZQ5sNHnlsKYHApQEcivKsruvn`
-    )
-      .then(function (responseTicket) {
-        return responseTicket.json();
-      })
-      .then(function (ticketData) {
-        console.log(ticketData._embedded.events[0]);
-      });
-  });
-
 var datePickerEl = $(".datePicker");
 var dropDownEl = $(".dropdownContent");
+var daysDiv = $(".days-div");
 
-  fetch(
-    'http://api.weatherstack.com/forecast?access_key=43c0e4c4f816398c7b97a3b59817de2b&query='+encodeURIComponent('New York')+'&forecast_days=1'
-  )
-  .then(function (responseWeather) {
-    return responseWeather.json();
-  })
-  .then(function (weatherData){
-    console.log(weatherData);
-  }) 
+const api_url =
+  "https://app.ticketmaster.com/discovery/v2/events.json?{id}/images&countryCode=US&city&radius&genre&apikey=D9il0k2ZQ5sNHnlsKYHApQEcivKsruvn";
+
+async function getData() {
+  const response = await fetch(api_url);
+  const ticketData = await response.json();
+  console.log(ticketData);
+  console.log(response);
+
+  daysDiv.empty();
+
+  daysDiv.each((index, element) => {
+    daysDiv.css("background-color", "rgb(245, 240, 233)");
+    element = $(element);
+    index = index;
+
+    element.empty();
+
+    element.css("width", "100%");
+    element.css("height", "300px");
+
+    var promoter = $("<div>");
+    promoter.text(ticketData._embedded.events[index].promoter.name);
+
+    promoter.css("color", "grey");
+    element.append(promoter);
+
+    var dateDiv = $("<div>");
+    dateDiv.text(ticketData._embedded.events[index].dates.timezone);
+    dateDiv.css("color", "grey");
+    element.append(dateDiv);
+
+    var nameDiv = $("<div>");
+    nameDiv.text(ticketData._embedded.events[index].name);
+    nameDiv.css("color", "grey");
+    element.append(nameDiv);
+
+    var venueDiv = $("<div>");
+    venueDiv.text(ticketData._embedded.events[index]._embedded.venues[0].name);
+    venueDiv.css("color", "grey");
+    element.append(venueDiv);
+  });
 }
 
-// getData();
-
-submitBtn.on('click',getData)
+submitBtn.on("click", getData);
