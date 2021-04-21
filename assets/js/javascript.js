d61efc5c0773e559ac2a6c80e9688ac0d65b1b8b
Brendan Tyler <bkrendan12@gmail.com>
2021-04-21 17:10:03 -0400

    split the weather and ticketmaster data into their own seperate div containers so the first row has our concerts and the second row has this week's forecast. I also replaced the dropdown menu with a form that works the same as before, so no functionality differences there

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,8 +3,9 @@ var submitBtn = $(".submitBtn");
 var genreSearch = $("#genre-input");
 var citySearch = $("#city-input");
 var daysDiv = $(".days-div");
+var weatherDivs = $(".weather-div");
 var dropdownBtn = $(".dropdown-content");
-var genreSelect = "";
+var genreSelect = $(".genre-select");
 var dropdownSelection = $("#dropdown-selection");
 
 var tempAr = [];
@@ -15,25 +16,9 @@ var tempAr = [];
 // 	localStorage.setItem('savedEvents', JSON.stringify(savedEvents));
 // }
 
-//DOMContentLoaded - it fires when initial HTML document has been completely loaded
-document.addEventListener("DOMContentLoaded", function () {
-  // querySelector - it returns the element within the document that matches the specified selector
-  var dropdown = document.querySelector(".dropdown");
-  //addEventListener - attaches an event handler to the specified element.
-  dropdown.addEventListener("click", function (event) {
-    //event.stopPropagation() - it stops the bubbling of an event to parent elements, by preventing parent event handlers from being executed
-    event.stopPropagation();
-    //classList.toggle - it toggles between adding and removing a class name from an element
-    dropdown.classList.toggle("is-active");
-  });
-});
-
 const api_url =
   "https://app.ticketmaster.com/discovery/v2/events.json?{id}/images&countryCode=US&apikey=D9il0k2ZQ5sNHnlsKYHApQEcivKsruvn&sort=date,asc";
 
-dropdownBtn.on("click", "a", function () {
-  genreSelect = $(this).text();
-});
 const weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 
 const oneCallURL = "https://api.openweathermap.org/data/2.5/onecall?";
@@ -44,7 +29,7 @@ async function getData() {
   tempAr = [];
   await getWeather();
   const response = await fetch(
-    `${api_url}&city=${citySearch.val()}&classificationName=${genreSelect.trim()}`
+    `${api_url}&city=${citySearch.val()}&classificationName=${genreSelect.val()}`
   );
   const ticketData = await response.json();
   console.log(ticketData);
@@ -52,6 +37,39 @@ async function getData() {
   var events = ticketData._embedded.events;
 
   console.log(events.length);
+
+  weatherDivs.empty();
+  weatherDivs.each((index, element) => {
+    element = $(element);
+    element.empty();
+
+    element.css({
+      display: "flex",
+      "flex-direction": "column",
+      "justify-content": "start",
+      "text-align": "center",
+      padding: "30px",
+      width: "100%",
+      height: "auto",
+      color: "grey",
+      "background-color": "rgb(245, 240, 233)",
+    });
+
+    var dateDiv = $("<div>");
+    var dateEl = $("<p>").css({ "font-size": "large", "font-weight": "bold" });
+    dateEl.text(
+      dayjs()
+        .add(index + 1, "day")
+        .format("MMMM D, YYYY")
+    );
+    element.append(dateDiv);
+    dateDiv.append(dateEl);
+    var weatherDiv = $("<div>");
+    var tempEl = $("<p>");
+    tempEl.text(tempAr[index]);
+    element.append(weatherDiv);
+    weatherDiv.append(tempEl);
+  });
 
   daysDiv.empty();
 
@@ -78,23 +96,13 @@ async function getData() {
       "background-color": "rgb(245, 240, 233)",
     });
 
-    var dateDiv = $("<div>");
-    var dateEl = $("<p>").css({"font-size": "large", "font-weight": "bold"})
-    dateEl.text(dayjs().add((index+1),'day').format('MMMM D, YYYY'));
-    element.append(dateDiv)
-    dateDiv.append(dateEl)
-
-    var weatherDiv = $("<div>");
-    var tempEl = $("<p>")
-    tempEl.text(tempAr[index]);
-    element.append(weatherDiv)
-    dateDiv.append(tempEl)
-
     var nameDiv = $("<div>");
+    nameDiv.css({ "font-size": "large", "font-weight": "bold" });
     nameDiv.text(events[index]._embedded.attractions[0].name);
     element.append(nameDiv);
 
     var concertDiv = $("<img>");
+    concertDiv.css({ "margin-top": "12px", "margin-bottom": "12px" });
     concertDiv.attr("src", `${events[index].images[index].url}`);
     console.log(concertDiv);
     element.append(concertDiv);
@@ -114,12 +122,10 @@ async function getData() {
     ticketDiv.attr("target", "_blank");
     element.append(ticketDiv);
   });
-
-  
 }
 
 function getWeather() {
-  return new Promise((resolve,reject) => {
+  return new Promise((resolve, reject) => {
     console.log(
       `${weatherURL}${encodeURIComponent(
         citySearch.val()
@@ -147,21 +153,20 @@ function getWeather() {
             return forecastResponse.json();
           })
           .then(function (forecast) {
-            console.log(forecast.daily)
+            console.log(forecast.daily);
             for (let i = 1; i < forecast.daily.length; i++) {
               console.log(forecast.daily[i].temp.eve + " \u00B0F");
               console.log(forecast.daily[i].pop + "%");
               console.log(forecast.daily[i].pop * 100 + "%");
               console.log(forecast.daily[i].weather[0].id);
-              tempAr.push(forecast.daily[i].temp.eve + " \u00B0F")
+              tempAr.push(forecast.daily[i].temp.eve + " \u00B0F");
             }
-            console.log(tempAr)
+            console.log(tempAr);
             resolve();
           });
       });
-
-  })
-};
+  });
+}
 
 submitBtn.on("click", getData);
 
