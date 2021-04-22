2f69b6ac4d97a7e2e227bce6254069bcedf458cd
Tracy Angell <63623364+tracye1083@users.noreply.github.com>
2021-04-21 23:02:16 -0400

    js and html updates
    Update to JS file to fix error with name search which stopped script from proceeding.
    updates to html box style to tile divs

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -15,7 +15,7 @@ var dropdownSelection = $("#dropdown-selection");
 // }
 
 const api_url =
-  "https://app.ticketmaster.com/discovery/v2/events.json?{id}/images&countryCode=US&apikey=D9il0k2ZQ5sNHnlsKYHApQEcivKsruvn&sort=date,asc&size=5";
+    "https://app.ticketmaster.com/discovery/v2/events.json?{id}/images&countryCode=US&apikey=D9il0k2ZQ5sNHnlsKYHApQEcivKsruvn&sort=date,asc&size=5";
 
 const weatherURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline`;
 
@@ -23,105 +23,105 @@ const weatherKey = "3AP2W24SGPFQY452VWE3UJ6UD"
 
 async function getData() {
 
-  const response = await fetch(
-    `${api_url}&city=${citySearch.val()}&classificationName=${genreSelect.val()}`
-  );
-  const ticketData = await response.json();
-  console.log(ticketData);
-  console.log(response);
-  var events = ticketData._embedded.events;
+    const response = await fetch(
+        `${api_url}&city=${citySearch.val()}&classificationName=${genreSelect.val()}`
+    );
+    const ticketData = await response.json();
+    console.log(ticketData);
+    console.log(response);
+    var events = ticketData._embedded.events;
 
-  console.log(events.length);
+    console.log(events.length);
 
 
-  daysDiv.empty();
+    daysDiv.empty();
 
-  daysDiv.each((index, element) => {
-    element = $(element);
+    daysDiv.each((index, element) => {
+        element = $(element);
 
-    element.empty();
+        element.empty();
 
-    if (events.length <= index) {
-      var sorryDiv = $("<div>");
-      sorryDiv.css({
-        display: "flex",
-        "flex-direction": "column",
-        "justify-content": "start",
-        "text-align": "center",
-        padding: "30px",
-        width: "100%",
-        height: "fit-content",
-        color: "grey",
-        "background-color": "rgb(245, 240, 233)",
-      });
-      sorryDiv.text("NOT AVAILABLE");
-      element.append(sorryDiv);
-      return;
-    }
-    element.css({
-      display: "flex",
-      "flex-direction": "column",
-      "justify-content": "start",
-      "text-align": "center",
-      padding: "30px",
-      width: "100%",
-      height: "auto",
-      color: "grey",
-      "background-color": "rgb(245, 240, 233)",
-    });
-
-    var nameDiv = $("<div>");
-    nameDiv.css({ "font-size": "large", "font-weight": "bold" });
-    nameDiv.text(events[index]._embedded.attractions[0].name);
-    element.append(nameDiv);
-
-    var concertDiv = $("<img>");
-    concertDiv.css({ "margin-top": "12px", "margin-bottom": "12px" });
-    concertDiv.attr("src", `${events[index].images[index].url}`);
-    console.log(concertDiv);
-    element.append(concertDiv);
-
-    var dateDiv = $("<div>");
-    var date = new Date(events[index].dates.start.dateTime);
-    dateDiv.text(date.toString().split(" ").slice(0, 4).join(" "));
-    element.append(dateDiv);
-
-    var timeDiv = $("<div>");
-    timeDiv.text(events[index].dates.localTime);
-    element.append(timeDiv);
-
-    var venueDiv = $("<div>");
-    venueDiv.text(events[index]._embedded.venues[0].name);
-    element.append(venueDiv);
-
-    var ticketDiv = $("<a>");
-    ticketDiv.text("TICKETS");
-    ticketDiv.attr("href", events[index].url);
-    ticketDiv.attr("target", "_blank");
-    element.append(ticketDiv);
-
-    fetch(
-      `${weatherURL}/${encodeURIComponent(citySearch.val())}/${events[index].dates.start.localDate}?key=${weatherKey}`
-    )
-      .then(function (weatherResponse) {
-        if (!weatherResponse.ok) {
-          alert("City not found... Try again");
+        if (events.length <= index) {
+            var sorryDiv = $("<div>");
+            sorryDiv.css({
+                display: "flex",
+                "flex-direction": "column",
+                "justify-content": "start",
+                "text-align": "center",
+                padding: "30px",
+                width: "100%",
+                height: "fit-content",
+                color: "grey",
+                "background-color": "rgb(245, 240, 233)",
+            });
+            sorryDiv.text("NOT AVAILABLE");
+            element.append(sorryDiv);
+            return;
         }
-        return weatherResponse.json();
-      })
-      .then(function (forecast) {
+        element.css({
+            display: "flex",
+            "flex-direction": "column",
+            "justify-content": "start",
+            "text-align": "center",
+            padding: "30px",
+            width: "100%",
+            height: "auto",
+            color: "grey",
+            "background-color": "rgb(245, 240, 233)",
+        });
 
-        var weatherDiv = $("<div>");
-        var weatherEl = $("<p>").text(`Dress for ${forecast.days[0].temp} \u00B0F and ${forecast.days[0].conditions}`).css({ "font-style": "italic"});
-        element.append(weatherDiv);
-        weatherDiv.append(weatherEl);
+        var nameDiv = $("<div>");
+        nameDiv.css({ "font-size": "large", "font-weight": "bold" });
+        nameDiv.text(events[index].name);
+        element.append(nameDiv);
 
-      })
-    
-  });
-  //content is now displayed with the response we received after on click event handler
-  $(".results").show();
-  $(".weather-results").show();  
+        var concertDiv = $("<img>");
+        concertDiv.css({ "margin-top": "12px", "margin-bottom": "12px" });
+        concertDiv.attr("src", `${events[index].images[index].url}`);
+        console.log(concertDiv);
+        element.append(concertDiv);
+
+        var dateDiv = $("<div>");
+        var date = new Date(events[index].dates.start.dateTime);
+        dateDiv.text(date.toString().split(" ").slice(0, 4).join(" "));
+        element.append(dateDiv);
+
+        var timeDiv = $("<div>");
+        timeDiv.text(events[index].dates.localTime);
+        element.append(timeDiv);
+
+        var venueDiv = $("<div>");
+        venueDiv.text(events[index]._embedded.venues[0].name);
+        element.append(venueDiv);
+
+        var ticketDiv = $("<a>");
+        ticketDiv.text("TICKETS");
+        ticketDiv.attr("href", events[index].url);
+        ticketDiv.attr("target", "_blank");
+        element.append(ticketDiv);
+
+        fetch(
+                `${weatherURL}/${encodeURIComponent(citySearch.val())}/${events[index].dates.start.localDate}?key=${weatherKey}`
+            )
+            .then(function(weatherResponse) {
+                if (!weatherResponse.ok) {
+                    alert("City not found... Try again");
+                }
+                return weatherResponse.json();
+            })
+            .then(function(forecast) {
+
+                var weatherDiv = $("<div>");
+                var weatherEl = $("<p>").text(`Dress for ${forecast.days[0].temp} \u00B0F and ${forecast.days[0].conditions}`).css({ "font-style": "italic" });
+                element.append(weatherDiv);
+                weatherDiv.append(weatherEl);
+
+            })
+
+    });
+    //content is now displayed with the response we received after on click event handler
+    $(".results").show();
+    $(".weather-results").show();
 }
 
 //hides the content when page loads.
